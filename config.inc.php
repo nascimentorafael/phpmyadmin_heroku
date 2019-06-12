8358850488530cb579e5350821ef0b5b2627b2c8
Rafael <rafaell.nascimentoo@gmail.com>
2019-06-13 01:08:59 +0200

    fix auth type

--- config.inc.php
+++ config.inc.php
@@ -26,7 +26,7 @@ $i = 0;
  */
 $i++;
 /* Authentication type */
-$cfg['Servers'][$i]['auth_type'] = 'cookie';
+$cfg['Servers'][$i]['auth_type'] = 'config';
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = 'rafa-food-express.herokuapp.com';
 $cfg['Servers'][$i]['compress'] = false;
