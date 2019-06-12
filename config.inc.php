769e5678418b4205a1dd454b8cc9007fa6bdf764
Rafael <rafaell.nascimentoo@gmail.com>
2019-06-13 00:56:20 +0200

    add host

--- config.inc.php
+++ config.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'rafa-food-express.herokuapp.com';
+$cfg['Servers'][$i]['host'] = 'https://rafa-food-express.herokuapp.com';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
 
